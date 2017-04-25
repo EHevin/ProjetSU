171ec4505b7db3a109ac2708ab4c7d70af8f312e
Erwan69 <erwan.hevin@gmail.com>
2017-04-25 23:23:07 +0200

    insertion maquett
    Please enter the commit message for your changes. Lines starting

---
+++ Symfony/src/AppBundle/Controller/ProjetController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProjetController extends Controller
+{
+    /**
+     * @Route("/index", name="accueil")
+     */
+    public function indexAction()
+    {
+      return $this->render('default/index.html.twig', [
+          'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+      ]);
+    }
+
+}
